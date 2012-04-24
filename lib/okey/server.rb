29b79c9b4750ee955020f3d995b210ff09a93b6d
Irmak Can <irmakcan@gmail.com>
2012-04-25 02:24:30 +0300

    Inactivity timeout added

--- lib/okey/server.rb
+++ lib/okey/server.rb
@@ -10,11 +10,12 @@ module Okey
     def initialize(opts = {})
       # @room_factory = RoomFactory.new # TODO
 
-      @env        = (opts.delete(:env) || Okey.env).to_sym
-      @host       = (opts.delete(:host) || '0.0.0.0').to_s
-      @ws_host    = (opts.delete(:ws_host) || '0.0.0.0').to_s
-      @ws_port    = (opts.delete(:ws_port) || 8080).to_i
-      @http_port  = (opts.delete(:http_port) || 3000).to_i
+      @env                = (opts.delete(:env) || Okey.env).to_sym
+      @host               = (opts.delete(:host) || '0.0.0.0').to_s
+      @ws_host            = (opts.delete(:ws_host) || '0.0.0.0').to_s
+      @ws_port            = (opts.delete(:ws_port) || 8080).to_i
+      @http_port          = (opts.delete(:http_port) || 3000).to_i
+      @inactivity_timeout = (opts.delete(:inactivity_timeout) || 300).to_i
 
       @@version    = (opts.delete(:version) || '0.0.0').to_s
 
@@ -34,6 +35,7 @@ module Okey
 
         EventMachine::WebSocket.start(:host => @ws_host, :port => @ws_port, :debug => @debug) do |ws|
           ws.onopen do
+            ws.comm_inactivity_timeout=@inactivity_timeout
             user = User.new(ws)
             @user_controller.subscribe(user)
           end
